2e9e9846ee4a798f36c68f3ee95a17b79cd9151f
AppNU <arifinsani40@gmail.com>
2023-05-21 13:46:07 +0700

    tambah data anggota

---
+++ app/src/main/java/com/example/login/DataAnggota.kt
@@ -0,0 +1,11 @@
+package com.example.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DataAnggota : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.data_anggota)
+    }
+}
