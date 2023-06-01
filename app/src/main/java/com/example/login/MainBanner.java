4849eccadfc4a7ebb397d190568194fb50b23fcc
AppNU <arifinsani40@gmail.com>
2023-06-01 14:13:57 +0700

    viewpdf

--- app/src/main/java/com/example/login/MainBanner.java
+++ app/src/main/java/com/example/login/MainBanner.java
@@ -4,22 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
 import java.util.ArrayList;
 
 public class MainBanner extends AppCompatActivity {
-//    ImageSlider imageSlider;
+    ImageSlider imageSlider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main_banner);
-//        imageSlider =findViewById(R.id.image_slider);
+        imageSlider =findViewById(R.id.image_slider);
 
-//        ArrayList<SlideModel> imageList = new ArrayList<>();
+        ArrayList<SlideModel> imageList = new ArrayList<>();
 
 
-//        imageList.add(new SlideModel(R.drawable.logo_ipnu,null));
-//        imageList.add(new SlideModel(R.drawable.logo_ippnu,"Add Title here",null));
-//        imageList.add(new SlideModel(R.drawable.logo_muslimat,null));
-//        imageSlider.setImageList(imageList, ScaleTypes.CENTER_CROP);
+        imageList.add(new SlideModel(R.drawable.logo_ipnu,null));
+        imageList.add(new SlideModel(R.drawable.logo_ippnu,null));
+        imageList.add(new SlideModel(R.drawable.logo_muslimat,null));
+        imageSlider.setImageList(imageList, ScaleTypes.CENTER_CROP);
     }
 }
